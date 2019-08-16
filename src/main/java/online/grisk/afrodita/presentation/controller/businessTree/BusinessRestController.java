92bfaefafa40a457899fd76f2651712df5620e34
John <jonathan.pereira.ing@gmail.com>
2019-08-15 22:42:35 -0400

    .

--- src/main/java/online/grisk/afrodita/presentation/controller/businessTree/BusinessRestController.java
+++ src/main/java/online/grisk/afrodita/presentation/controller/businessTree/BusinessRestController.java
@@ -1,10 +1,36 @@
 package online.grisk.afrodita.presentation.controller.businessTree;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import online.grisk.afrodita.integration.activator.impl.TreeActivatorService;
 import online.grisk.afrodita.presentation.controller.BasicRestController;
 
 @RestController
 public class BusinessRestController extends BasicRestController {
 	
+	@Autowired
+	private TreeActivatorService treeActivatorService;
+
+	@PostMapping(value = "/v1/rest/tree")
+	public ResponseEntity<?> registerRiskRatio(@Valid @RequestBody Map payload, Errors errors) throws Exception {
+		if (errors.hasErrors()) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		this.verifyParameters(payload);
+		Map response = treeActivatorService.invokeRegisterTree(payload, new HashMap());
+
+		return new ResponseEntity<>(response,
+				HttpStatus.valueOf(Integer.parseInt(response.getOrDefault("status", "500").toString())));
+	}
 }
